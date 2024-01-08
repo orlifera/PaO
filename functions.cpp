af6e7a3fc9fc79cc39a0fb280b3b6d1fb316a7ae
Dona-Da <davide.dona02@gmail.com>
2024-01-08 16:35:05 +0100

    fatto quasi tutto il backend in rust

--- functions.cpp
+++ functions.cpp
@@ -1,69 +1,105 @@
 #include"functions.h"
 
-Data::Data(double val, string dat) : value(val), date(dat) {}
-double Data::getValue() const { return value; }
-string Data::getDate() const { return date; }
+Time::Time(unsigned int h, unsigned int m, unsigned int s) {
+    if (h > 23 || m > 59 || s > 59) sec = 0;
+    else sec = h * 3600 + m * 60 + s;
+}
+unsigned int Time::Hour() const { return sec / 3600; }
+unsigned int Time::Mins() const { return (sec / 60) % 60; }
+unsigned int Time::Secs() const { return sec % 60; }
+//Time::operator int() { return sec; }
+ostream& operator<<(ostream& os, const Time& t) {
+    return os << t.Hour() << ":" << t.Mins() << ":" << t.Secs();
+}
 
-//Sensor::Sensor(string n, vector<Data> v) : name(n), infoArray(new vector<Data>(v)) {}
-//Sensor::Sensor(const Sensor& s) { name = s.getName(); infoArray = new vector<Data>(s.getArray()) }
+Data::Data(double val, Time t) : value(val), time(t) {}
+double Data::getValue() const { return value; }
+Time Data::getTime() const { return time; }
+
+Sensor::Sensor(string n, vector<Data> v) : name(n), infoArray(v) {}
 string Sensor::getName() const { return name; }
 vector<Data> Sensor::getArray() const { return infoArray; }
 Data Sensor::getInfo(int i) const { return infoArray[i]; }
-void Sensor::addData(Data d, int pos) { /*rivedere funzionamento di [] nei vettori*/ }
-virtual void Sensor::removeData(Data d, int pos) { /*same*/ }
-//virtual Sensor::~Sensor() { delete infoArray; }
+void Sensor::addData(Data d) { infoArray.push_back(d); }
+void Sensor::removeData() { infoArray.pop_back(); }
+void Sensor::renameSensor(string n) { name = n; } 
 
-//costruttore, copia e distruttore di temp e derivati
+TemperatureSensor::TemperatureSensor(string n, vector<Data> v, bool c, double t) : Sensor(n,v), contact(c), temp(t) {}
 bool TemperatureSensor::isContact() const { return contact; }
 double TemperatureSensor::getTemp() const { return temp; }
 void TemperatureSensor::setTemp(double t) { temp = t; }
 
-int VinesTemperatureSensor::isInThreshold() const override {
-    double last = infoArray[infoArray.size()-1];
-    if (last > vineThreshold+temp) return 1;
-    else if (last < temp-vineThreshold) return -1;
+VinesTemperatureSensor::VinesTemperatureSensor(string n, vector<Data> v, double t) : TemperatureSensor(n,v,false,t) {}
+int VinesTemperatureSensor::isInThreshold() const {
+    double last = getInfo(getArray().size()-1).getValue();
+    if (last > vineThreshold+getTemp()) return 1;
+    else if (last < getTemp()-vineThreshold) return -1;
     else return 0;
 }
+double VinesTemperatureSensor::vineThreshold = 0;
 
-int WineryTemperatureSensor::isInThreshold() const override {
-    double last = infoArray[infoArray.size()-1];
-    if (last > wineryThreshold+temp) return 1;
-    else if (last < temp-wineThreshold) return -1;
+MustTemperatureSensor::MustTemperatureSensor(string n, vector<Data> v, double t) : TemperatureSensor(n,v,true,t) {}
+int MustTemperatureSensor::isInThreshold() const {
+    double last = getInfo(getArray().size()-1).getValue();
+    if (last > mustThreshold+getTemp()) return 1;
+    else if (last < getTemp()-mustThreshold) return -1;
     else return 0;
 }
+double MustTemperatureSensor::mustThreshold = 0;
 
-int MustTemperatureSensor::isInThreshold() const override {
-    double last = infoArray[infoArray.size()-1];
-    if (last > mustThreshold+temp) return 1;
-    else if (last < temp-mustThreshold) return -1;
-    else return 0;
-}
-
-//costruttore, copia e distruttore di humid e derivati
+HumiditySensor::HumiditySensor(string n, vector<Data> v, bool c, double h) : Sensor(n,v), capacity(c), humidity(h) {}
 bool HumiditySensor::isCapacity() const { return capacity; }
 double HumiditySensor::getHumidity() const { return humidity; }
 void HumiditySensor::setHumidity(double h) { humidity = h; }
 
+AirHumiditySensor::AirHumiditySensor(string n, vector<Data> v, double h) : HumiditySensor(n,v,true,h) {}
+int AirHumiditySensor::isInThreshold() const {
+    double last = getInfo(getArray().size()-1).getValue();
+    if (last > airThreshold+getHumidity()) return 1;
+    else if (last < getHumidity()-airThreshold) return -1;
+    else return 0;
+}
+double AirHumiditySensor::airThreshold = 0;
 
+SoilHumiditySensor::SoilHumiditySensor(string n, vector<Data> v, double h) : HumiditySensor(n,v,false,h) {}
+int SoilHumiditySensor::isInThreshold() const {
+    double last = getInfo(getArray().size()-1).getValue();
+    if (last > soilThreshold+getHumidity()) return 1;
+    else if (last < getHumidity()-soilThreshold) return -1;
+    else return 0;
+}
+double SoilHumiditySensor::soilThreshold = 0;
 
-//costruttore, copia e distruttore di pressure e derivati
+PressureSensor::PressureSensor(string n, vector<Data> v, double p) : Sensor(n,v), pressure(p) {}
 double PressureSensor::getPressure() const { return pressure; }
 void PressureSensor::setPressure(double p) { pressure = p; }
 
-//Group::Group(string n, vector<Sensor*> s) : groupName(n), sensors(new vector<Sensor*>(s)) {}
-//Group::Group(const Group& g) { groupName = g.getGroupName(); sensors = new vector<Sensor*>(g.getSensors()) }
+AtmPressureSensor::AtmPressureSensor(string n, vector<Data> v) : PressureSensor(n,v,1.0) {}
+int AtmPressureSensor::isInThreshold() const {
+    double last = getInfo(getArray().size()-1).getValue();
+    if (last > atmThreshold+getPressure()) return 1;
+    else if (last < getPressure()-atmThreshold) return -1;
+    else return 0;
+}
+double AtmPressureSensor::atmThreshold = 0;
+
+int BarrelSensor::isInThreshold() const {
+    double last = getInfo(getArray().size()-1).getValue();
+    if (last > barrelThreshold+getPressure()) return 1;
+    else if (last < getPressure()-barrelThreshold) return -1;
+    else return 0;
+}
+double BarrelSensor::barrelThreshold = 0;
+
+Group::Group(string n, vector<Sensor*> s) : groupName(n), sensors(s) {}
 string Group::getGroupName() const { return groupName; }
 vector<Sensor*> Group::getSensors() const { return sensors; }
-void Group::addSensor(Sensor* s, int pos) { /*no idea*/ }
-void Group::removeSensor(Sensor* s, int pos) { /*no idea*/ }
-Sensor Group::find(string n) const {
+void Group::addSensor(Sensor* s) { sensors.push_back(s); }
+void Group::removeSensor() { sensors.pop_back(); }
+Sensor* Group::find(string n) const {
     for (auto it = sensors.begin(); it != sensors.end(); it++) {
-        if (it*->getName() == n) return it**;
+        //if ((it*).getName() == n) return it*;
     }
 }
-void Group::rename(string n) { groupName == n; }
+void Group::renameGroup(string n) { groupName == n; }
 //no fucking clue
-//Group& Group::operator=() {}
-//Group& Group::operator+() {}
-//Group& Group::operator-() {}
-//Group::~Group() { delete sensors; }
--- functions.h
+++ functions.h
@@ -9,18 +9,30 @@ using std::cerr;
 using std::endl;
 using std::string;
 using std::vector;
+using std::ostream;
+
+class Time {
+    private:
+    unsigned int sec;
+    public:
+    Time(unsigned int =0, unsigned int =0, unsigned int =0);
+    unsigned int Hour() const;
+    unsigned int Mins() const;
+    unsigned int Secs() const;
+    //operator int();
+};
 
 class Data {
     private:
     double value;
-    string date;
+    Time time;
     public:
     //Si potrebbe fare in modo che una funzione
     //generi randomicamente o secondo una distribuzione
     //il valore da assegnare al dato.
-    Data(double, string);
+    Data(double, Time);
     double getValue() const;
-    string getDate() const;
+    Time getTime() const;
 };
 
 class Sensor {
@@ -30,14 +42,14 @@ class Sensor {
     public:
     //Da controllare il costruttore e costruttore di copia.
     Sensor(string, vector<Data>);
-    Sensor(const Sensor&);
     Data getInfo(int) const;
     string getName() const;
     vector<Data> getArray() const;
-    void addData(Data, int);
-    void removeData(Data, int);
+    void addData(Data);
+    void removeData();
+    void renameSensor(string);
     virtual int isInThreshold() const = 0;
-    virtual ~Sensor() = 0;
+    virtual ~Sensor() = default;
 };
 
 class TemperatureSensor : public Sensor {
@@ -46,7 +58,6 @@ class TemperatureSensor : public Sensor {
     double temp;
     public: 
     TemperatureSensor(string, vector<Data>, bool, double);
-    TemperatureSensor(const TemperatureSensor&);
     bool isContact() const;
     double getTemp() const;
     void setTemp(double); 
@@ -56,76 +67,67 @@ class VinesTemperatureSensor : public TemperatureSensor {
     private:
     static double vineThreshold;
     public:
-    VinesTemperatureSensor(string, vector<Data>);
+    VinesTemperatureSensor(string, vector<Data>, double);
     int isInThreshold() const override;
 };
-
-double VinesTemperatureSensor::vineThreshold = 0;
-
-class WineryTemperatureSensor : public TemperatureSensor {
-    private:
-    static double wineryThreshold;
-    public:
-    WineryTemperatureSensor(string, vector<Data>);
-    int isInThreshold() const override;
-};
-
-double VinesTemperatureSensor::wineryThreshold = 0;
 
 class MustTemperatureSensor : public TemperatureSensor {
     private:
     static double mustThreshold;
     public:
-    MustTemperatureSensor(string, vector<Data>);
+    MustTemperatureSensor(string, vector<Data>, double);
     int isInThreshold() const override;
 };
-
-double VinesTemperatureSensor::mustThreshold = 0;
 
 class HumiditySensor : public Sensor {
     private:
     const bool capacity;
     double humidity;
     public:
-    HumiditySensor(string, vector<Data>, bool);
-    HumiditySensor(const HumiditySensor&);
+    HumiditySensor(string, vector<Data>, bool, double);
     bool isCapacity() const;
     double getHumidity() const;
     void setHumidity(double);
-    virtual bool isInHumidity(Data) const = 0;
 };
 
 class AirHumiditySensor : public HumiditySensor {
+    private:
+    static double airThreshold;
     public:
-    AirHumiditySensor(string, vector<Data>);
-    bool isInHumidity(Data) override const;
+    AirHumiditySensor(string, vector<Data>, double);
+    int isInThreshold() const override;
 };
 
 class SoilHumiditySensor : public HumiditySensor {
+    private:
+    static double soilThreshold;
     public:
-    SoilHumiditySensor(string, vector<Data>);
-    bool isInHumidity(Data) override const;
+    SoilHumiditySensor(string, vector<Data>, double);
+    int isInThreshold() const override;
 };
 
 class PressureSensor : public Sensor {
     private:
     double pressure;
     public:
+    PressureSensor(string, vector<Data>, double);
     double getPressure() const;
     void setPressure(double);
-    virtual bool isInPressure(Data) const = 0;
 };
 
 class AtmPressureSensor : public PressureSensor {
+    private:
+    static double atmThreshold;
     public:
     AtmPressureSensor(string, vector<Data>);
-    bool isInPressure(Data) override const;
+    int isInThreshold() const override;
 };
 
 class BarrelSensor : public PressureSensor {
+    private:
+    static double barrelThreshold;
     public:
-    BarrelSensor(string, vector<Data>);
-    bool isInPressure(Data) override const;
+    int isInThreshold() const override;
 };
 
 class Group {
@@ -134,15 +136,13 @@ class Group {
     vector<Sensor*> sensors;
     public:
     Group(string, vector<Sensor*>);
-    Group(const Group&);
     string getGroupName() const;
     vector<Sensor*> getSensors() const;
-    void addSensor(Sensor*, int);
-    void removeSensor(Sensor*, int);
-    Sensor find(string) const;
-    void rename(string);
-    Group& operator=();
-    ~Group();
+    void addSensor(Sensor*);
+    void removeSensor();
+    Sensor* find(string) const;
+    void renameGroup(string);
+    //Group& operator=(const Group&);
 };
 
 #endif
